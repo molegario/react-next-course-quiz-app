46d3433897cb532c7ca9630d5897ff9986c4b101
molegario <molegario@x2omedia.com>
2025-03-15 00:24:07 -0400

    fixes to question timer

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,33 +1,48 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { QuizContext } from "../context/quiz-context";
 
 const QuestionTimer = ({
   duration = 10 * 1000,
 }) => {
-  const { handleSkipAnswer } = useContext(QuizContext);
+  const timerRef = useRef();
+  const intervalRef = useRef();
+  const { handleSkipAnswer, answeredState } = useContext(QuizContext);
   const [timerRemaining, setTimeRemaining] = useState(duration);
 
+  useEffect(
+    () => {
+      if (answeredState !== "") {
+        clearTimeout(timerRef.current);
+      }
+    },
+    [answeredState]
+  )
+
   useEffect(() => {
-    const timer = setTimeout(() => handleSkipAnswer(), duration);
+    timerRef.current = setTimeout(() => handleSkipAnswer(), duration);
     return () => {
-      clearTimeout(timer);
+      clearTimeout(timerRef.current);
     };
   }, [handleSkipAnswer, duration]);
 
   useEffect(
     () => {
-      const intervalTimer = setInterval(
+      intervalRef.current = setInterval(
         () => {
           setTimeRemaining(
             prevTime => {
-              return prevTime - 100;
+              const newTime = prevTime - 100;
+              if(newTime <= 0) {
+                clearInterval(intervalRef.current);
+              }
+              return newTime < 0 ? 0 : newTime;
             }
           );
         },
         100
       );
       return () => {
-        clearInterval(intervalTimer);
+        clearInterval(intervalRef.current);
       };
     },
     []
